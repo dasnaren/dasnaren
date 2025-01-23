f612db87a246e270daaeb41174a49f97508d110b
Naren <wbnaren@gmail.com>
2025-01-23 22:23:41 +0530

    Cart Design

---
+++ cartdesign/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center">
+        <div className="bg-green-500 text-white p-5 rounded-lg w-1/2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+          facere quos exercitationem beatae voluptas nostrum, at explicabo,
+          ratione, corrupti sequi consequuntur obcaecati asperiores. Tempore
+          provident, ipsa quae asperiores placeat a.
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
