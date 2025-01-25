4a6600dfbc26e86a3ec819c6859d99dcf2ee2ead
Naren <wbnaren@gmail.com>
2025-01-25 21:02:21 +0530

    Formik all type field form handeling

---
+++ formwithtailwind/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Registration from "./assets/component/Registration";
+
+function App() {
+  return (
+    <>
+      <Registration />
+    </>
+  );
+}
+
+export default App;
